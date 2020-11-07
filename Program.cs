c935b840fefdd602738742301c1cab26c0800671
Levi Fichman <61022686+LTheCoder@users.noreply.github.com>
2020-11-07 21:20:37 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Bus_project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+    class Bus
+    {
+        static int fuel = 0;
+        static int dis = 0;
+        static int ID = 0;
+        static int start_date = 0;
+    }
+}
